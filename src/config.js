ab09f973ccd496f4f321e79760acd123f52bdf7d
Jie <jie@Jies-MacBook-Pro.local>
2020-07-25 22:00:32 -0500

    Setting up aws connection

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-20200723",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://g5me0vbfa7.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_fBaSwe6yE",
+    APP_CLIENT_ID: "10n8aur7bvssp5po5j486kkch4",
+    IDENTITY_POOL_ID: "us-east-1:cf0e2a96-4430-494c-bca3-5de486312267",
+  },
+};
